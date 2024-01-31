bfcd1a5f46000f06296b6f30600bcc495e17d4ba
zvic81 <110486938+zvic81@users.noreply.github.com>
2024-01-31 15:58:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let counter = 0;
 
 const app = express();
 app.get('/', (req, res) => {
-  const message = `Hello from Vic, I'm from Apatity, it's cold now and today is  ${new Date()}  Testing trigger from terraform!!!!!!!!!!`;
+  const message = `Hello from Vic, I'm from Apatity, it's cold now and today is  ${new Date()}  Testing trigger from terraform. And test variables for terraform yet`;
   console.log(message);
   res.send(message);
 });
